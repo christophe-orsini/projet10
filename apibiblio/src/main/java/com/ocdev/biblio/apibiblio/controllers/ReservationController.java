38d190c93363d345612997a3aa5ef052740ab6fc
Christophe <christophe.orsini@gmail.com>
2021-01-28 16:24:20 +0100

    Création d'un controleur spécifique pour les réservations

---
+++ apibiblio/src/main/java/com/ocdev/biblio/apibiblio/controllers/ReservationController.java
@@ -0,0 +1,48 @@
+package com.ocdev.biblio.apibiblio.controllers;
+
+
+import javax.validation.constraints.Min;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ocdev.biblio.apibiblio.entities.Pret;
+import com.ocdev.biblio.apibiblio.errors.AlreadyExistsException;
+import com.ocdev.biblio.apibiblio.errors.EntityNotFoundException;
+import com.ocdev.biblio.apibiblio.errors.FullWaintingQueueException;
+import com.ocdev.biblio.apibiblio.errors.NotEnoughCopiesException;
+import com.ocdev.biblio.apibiblio.services.PretService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("/api/v1")
+@Validated
+public class ReservationController
+{
+	@Autowired PretService pretService;
+		
+	@ApiOperation(value = "Réservation d'un ouvrage", notes = "Réservation d'un ouvrage")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "La réservation est enregistrée"),
+			@ApiResponse(code = 403, message = "Authentification requise"),
+			@ApiResponse(code = 404, message = "L'abonné et/ou l'ouvrage n'existe pas"),
+			@ApiResponse(code = 460, message = "Un prêt en cours existe déjà pour cet ouvrage et cet abonné"),
+			@ApiResponse(code = 462, message = "Pas assez d'exemplaires pour la réservation de cet ouvrage"),
+			@ApiResponse(code = 463, message = "Nombre maximum de réservation atteint pour cet ouvrage")
+			})
+	@PutMapping(value = "/reservations/abonne/{abonneId}/ouvrage/{ouvrageId}", produces = "application/json" )
+	public ResponseEntity<Pret> reservation(@ApiParam(value = "ID de l'abonné", required = true, example = "1") @PathVariable @Min(1) final Long abonneId, 
+			@ApiParam(value = "ID de l'ouvrage", required = true, example = "1") @PathVariable @Min(1) final Long ouvrageId)
+					throws AlreadyExistsException, EntityNotFoundException, NotEnoughCopiesException, FullWaintingQueueException
+	{
+		Pret result = pretService.reserver(abonneId, ouvrageId);
+		return new ResponseEntity<Pret>(result, HttpStatus.CREATED);
+	}
+}
