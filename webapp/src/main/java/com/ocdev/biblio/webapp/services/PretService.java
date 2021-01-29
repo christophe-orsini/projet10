30526d4d016862d6490007d316c120ba0a7ea7df
Christophe <christophe.orsini@gmail.com>
2021-01-29 17:23:37 +0100

    Creation de la page listeReservation

--- webapp/src/main/java/com/ocdev/biblio/webapp/services/PretService.java
+++ webapp/src/main/java/com/ocdev/biblio/webapp/services/PretService.java
@@ -6,10 +6,12 @@ import javax.persistence.EntityNotFoundException;
 
 import org.springframework.data.domain.Page;
 
+import com.ocdev.biblio.webapp.dto.ReservationDto;
 import com.ocdev.biblio.webapp.objects.Pret;
 
 public interface PretService
 {
 	public Page<Pret> listePrets(Principal abonne, int page, int taille) throws EntityNotFoundException;
 	public Pret prolonger(Principal abonne, Long pretId) throws EntityNotFoundException;
+	public Page<ReservationDto> listeReservations(Principal abonne, int page, int taille) throws EntityNotFoundException;
 }
