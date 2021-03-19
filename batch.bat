edec53b6f773e424cf2620c7a29549d20d08d558
Christophe <christophe.orsini@gmail.com>
2021-03-19 21:22:59 +0100

    Création de la version

--- batch.bat
+++ batch.bat
@@ -1,12 +1,12 @@
 @echo off
 chcp 65001
 cls
-if not exist batch\target\batch-1.1.7.jar (
+if not exist batch\target\batch-1.2.7.jar (
 	echo **************************************************************************************
 	echo * L'application batch n'est pas installée !                                          *
 	echo * Exécuter la commande 'install' en mode console ou double-cliquer sur 'install.bat' *
 	echo * pour installer l'application                                                       *
 	echo **************************************************************************************
 ) else (
-	java -jar batch\target\batch-1.1.7.jar
+	java -jar batch\target\batch-1.2.7.jar
 )
